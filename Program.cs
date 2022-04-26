a7f09568307deeeb67bd7ed3eb64ad2dbf060c6c
aboriska <aboriska@yahoo.com>
2022-04-26 16:35:41 +0300

    Lesson-3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x1 = 1;
+int y1 = 3;
+char sym1 = '*';
+
+static void Draw(int x, int y, char sym)
+{
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(sym);
+}
+
+Draw(x1, y1, sym1);
